39e4b69562409bef49023ca8c6298ef7eee2b616
wzt <ray2879555324@foxmail.com>
2024-01-20 20:54:42 +0800

    en-zh change

--- web_demo.py
+++ web_demo.py
@@ -1,3 +1,5 @@
+from cProfile import label
+from multiprocessing.sharedctypes import Value
 import gradio as gr
 
 CSS = r"""
@@ -29,19 +31,40 @@ CSS = r"""
 """
 
 chat_templates = ['internlm2-chat-7b']
+def lang_change(lang):
+    if lang == "en":
+        return  gr.update(label='language'), \
+                gr.update(label='chat_TEMPLATE'), \
+                gr.update(label='model_path'), \
+                gr.update(label='predict_way'), \
+                gr.update(label='Chatbot'), \
+                gr.update(label='Textbox'), \
+                gr.update(value='Clear')
+    elif lang == "zh":
+        return  gr.update(label='语言'), \
+                gr.update(label='模型模板'), \
+                gr.update(label='模型路径'), \
+                gr.update(label='预测方式'), \
+                gr.update(label='聊天机器人'), \
+                gr.update(label='对话框'), \
+                gr.update(value='清除记录')
 
 with gr.Blocks(title="Xtuner Chat Board", css=CSS) as demo:
     gr.HTML(
         "<h1><center>Xtuner Chat Board</center></h1>"
     )
     with gr.Row():
-        lang = gr.Dropdown(label='langguage', choices=["en", "zh"], scale=1)
-        chat_template = gr.Dropdown(
-            label='CHAT_TEMPLATE', choices=chat_templates, scale=3)
+        lang = gr.Dropdown(label='language', choices=["en", "zh"], scale=1)
+        chat_TEMPLATE = gr.Dropdown(
+            label='chat_TEMPLATE', choices=chat_templates, scale=3)
         predict_way = gr.Dropdown(label='predict_way',choices=['HFBot','LMDeployBot','VllmBot','OpenaiBot'])
     model_path = gr.Textbox(label='model_path', scale=3)
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot(label='Chatbot')
     history = gr.State([])
+    msg = gr.Textbox(label='Textbox')
+    clear = gr.ClearButton([msg, chatbot], value='Clear')
+    lang.select(fn=lang_change, inputs=[lang], outputs=[lang, chat_TEMPLATE, model_path, predict_way, chatbot, msg, clear])
+
 
 demo.queue()
 demo.launch(server_name="0.0.0.0", share=False, inbrowser=True)
